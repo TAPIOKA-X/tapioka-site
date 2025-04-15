91ec6cc52776fb5265793d23b62b9f4e506ddb52
tapioka-jp <suetake0301@outlook.jp>
2025-04-15 18:54:59 +0900

    Create PrivateServerCode.js

---
+++ JavaScript/PrivateServerCode.js
@@ -0,0 +1,7 @@
+var PScodeBtn = document.getElementById("PScodeBtn");
+var PScode = document.getElementById("PScode");
+
+PScodeBtn.onclick = function(){
+ alert("更新しますか？");
+ PScode.innerHTML = "aoEkHih";
+}
