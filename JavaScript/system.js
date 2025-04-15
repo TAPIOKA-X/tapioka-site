db9903be3d42aa0a15afff36d52ebaeb8b8b0d79
tapioka-jp <suetake0301@outlook.jp>
2025-04-15 18:32:13 +0900

    Update system.js

--- JavaScript/system.js
+++ JavaScript/system.js
@@ -2,7 +2,7 @@ var PScodeBtn = document.getElementById('PScodeBtn');
 var PScode = document.getElemenetById('PScode')
 
 function NoFile(){
-  alert("次のアップデートまでに追加します..\nWill add it in the next update..")
+  alert("次のアップデートまでに追加します..\nWill add it in the next update..");
 }
 
 PScodeBtn.onclick = function(){
