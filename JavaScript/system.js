afff3a3190551c7be5428cdd673c5852e5cd6826
tapioka-jp <suetake0301@outlook.jp>
2025-04-15 18:24:44 +0900

    Update system.js

--- JavaScript/system.js
+++ JavaScript/system.js
@@ -1,6 +1,12 @@
-var AlertClick = document.getElementById('NoFile');
+var PScodeBtn = document.getElementById('PScodeBtn');
+var PScode = document.getElemenetById('PScode')
 
-AlertClick.onclick = function(){
+function NoFile(){
   alert("次のアップデートまでに追加します..\nWill add it in the next update..")
+}
+
+PScodeBtn.onclick = function(){
+ alert("更新しますか？");
+ PScode.innerHTML = 'aoEkHih';
 }
 
