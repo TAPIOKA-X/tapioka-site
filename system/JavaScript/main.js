e9e520b9dc82039dc0b5630d2aabc2e1d5362a3b
tapioka-jp <suetake0301@outlook.jp>
2025-04-19 17:52:00 +0900

    Update main.js

--- system/JavaScript/main.js
+++ system/JavaScript/main.js
@@ -1,12 +1,3 @@
-//Link System
-function UnwaveringSoul(){
- location.href= "https://tapioka-site.f5.si/Blog/Menu/UnwaveringSoul-Menu.html";
-}
-
-function Grace(){
- location.href= "https://tapioka-site.f5.si/Blog/Menu/Grace-Menu.html";
-}
-
 //Private Server Code System
 var Code = document.getElementById("PS-Code");
 
