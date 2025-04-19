51ce23eda18d6b137b0acad950ae081f2b0fa98e
tapioka-jp <suetake0301@outlook.jp>
2025-04-19 17:34:44 +0900

    Create main.js

---
+++ system/JavaScript/main.js
@@ -0,0 +1,13 @@
+//Private Server Code System
+var Code = document.getElementById("PS-Code");
+
+function CodeButton(){
+ alert("更新しますか？");
+ Code.innerHTML = "Pls wait..";
+}
+
+//Error404
+function Error404(){
+ alert(Error404);
+ location.href= "https://tapioka-site.f5.si/";
+}
