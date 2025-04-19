c82ec4be54bcf236a8b6600f3d6033fcaddcd328
tapioka-jp <suetake0301@outlook.jp>
2025-04-19 17:26:59 +0900

    Delete JavaScript/PrivateServerCode.js

--- JavaScript/PrivateServerCode.js
+++
@@ -1,7 +0,0 @@
-var PScodeBtn = document.getElementById("PScodeBtn");
-var PScode = document.getElementById("PScode");
-
-PScodeBtn.onclick = function(){
- alert("更新しますか？");
- PScode.innerHTML = "aoEkHih";
-}
