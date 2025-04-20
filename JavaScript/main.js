3962026ec5a0021ce925c157ccc02e03c6a2376b
tapioka-jp <suetake0301@outlook.jp>
2025-04-20 14:04:24 +0900

    Update main.js

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,8 +1,17 @@
+//Link
 function HomeLink(){
  location.href= "https://tapioka-site.f5.si/";
 }
 
+function USpsLink(){
+ location.href= "https://tapioka-site.f5.si/Blog/UnwaveringSoul/UnwaveringSoul-PS.html";
+}
 
+function USytbLink(){
+ location.href= "https://tapioka-site.f5.si/Blog/UnwaveringSoul/UnwaveringSoul-YouTuber.html";
+}
+
+//Alert
 function UpdateAlert(){
  alert("現在制作中です。");
  location.href= "https://tapioka-site.f5.si/";
