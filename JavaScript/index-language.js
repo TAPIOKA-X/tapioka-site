7e93d980e57a329eb6cde1633eb59e993a199822
tapioka-jp <suetake0301@outlook.jp>
2025-04-20 14:30:32 +0900

    Update index-language.js

--- JavaScript/index-language.js
+++ JavaScript/index-language.js
@@ -1,9 +1,11 @@
 var welcome = document.getElementById("Welcome");
 var USreset = document.getElementById("UnwaveringSoulReset");
+var USblogMenu = document.getElementById("UnwaveringSoulBlogMenu");
 
 function english(){
  alert("テスト段階の機能のため、バグがある可能性があります。実行しますか？");
  welcome.innerHTML = "Welcome";
  USreset.innerHTML = "Unwavering Soul Reset : 150Reset";
  
+ USblogMenu.innerHTML = "Open to Unwavering Soul Blog Menu";
 }
