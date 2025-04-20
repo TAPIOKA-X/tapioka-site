374db0bd6d0b43468b16108b23fc37b66b365abc
tapioka-jp <suetake0301@outlook.jp>
2025-04-20 15:29:48 +0900

    Update USps-language.js

--- JavaScript/Language/USps-language.js
+++ JavaScript/Language/USps-language.js
@@ -1,1 +1,6 @@
-
+var title = document.getElemntById("Title");
+var HomeLink = document.getElementById("HomeLink");
+function English(){
+ title.innerHTML = "Unwavering Soul Private Server";
+ HomeLink.innerHtML = "Go back home..";
+}
