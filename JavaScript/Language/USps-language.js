8232078c2b855f13888ab7f138561cfa4a448364
tapioka-jp <suetake0301@outlook.jp>
2025-04-20 15:43:32 +0900

    Update USps-language.js

--- JavaScript/Language/USps-language.js
+++ JavaScript/Language/USps-language.js
@@ -1,8 +1,14 @@
 var title = document.getElemntById("Title");
 var HomeLink = document.getElementById("Home");
 
+var PScode - document.getElementById("PScode");
+var PScodeTitle - document.getElementById("PScodeTitle");
+
 function English(){
  alert("テスト中の機能のため、バグが発生する可能性があります！");
  title.innerHTML = "Unwavering Soul Private Server";
- HomeLink.innerHtML = "Go back home..";
+ HomeLink.innerHTML = "Go back home..";
+
+ PScode.innerHTML = "Private Server Code is 「aoEkHih」";
+ PScodeTitle.innerHTML = "Private Server Code";
 }
