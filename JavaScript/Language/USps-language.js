804554186daa63f2cfefab3c2cd11d4cd9118112
tapioka-jp <suetake0301@outlook.jp>
2025-04-20 16:17:35 +0900

    Update USps-language.js

--- JavaScript/Language/USps-language.js
+++ JavaScript/Language/USps-language.js
@@ -5,7 +5,6 @@ var PScode = document.getElementById("PScode");
 var PScodeTitle = document.getElementById("PScodeTitle");
 
 function English(){
- alert("テスト中の機能のため、バグが発生する可能性があります！");
  document.title = "Unwavering Soul Private Server Code";
  PageTitle.innerHTML = "Unwavering Soul Private Server";
  HomeLink.innerHTML = "Home";
