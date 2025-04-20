6e4b695ed5823d5a3cd156ae630a366cc3dd8e5c
tapioka-jp <suetake0301@outlook.jp>
2025-04-20 16:09:51 +0900

    Update USps-language.js

--- JavaScript/Language/USps-language.js
+++ JavaScript/Language/USps-language.js
@@ -1,8 +1,8 @@
 var title = document.getElementById("Title");
 var HomeLink = document.getElementById("Home");
 
-var PScode - document.getElementById("PScode");
-var PScodeTitle - document.getElementById("PScodeTitle");
+var PScode = document.getElementById("PScode");
+var PScodeTitle = document.getElementById("PScodeTitle");
 
 function English(){
  alert("テスト中の機能のため、バグが発生する可能性があります！");
