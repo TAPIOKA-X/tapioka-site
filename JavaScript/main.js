1011c8a75b76b64517ff90f6b0b6302affe028ec
tapioka-jp <suetake0301@outlook.jp>
2025-04-24 19:07:15 +0900

    Update main.js

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -11,11 +11,6 @@ function USytbLink(){
  location.href= "https://tapioka-site.f5.si/Blog/UnwaveringSoul/UnwaveringSoul-YouTuber.html";
 }
 
-//Private Server Code Copy
-function PScodeCpoy(){
- navigator.clipboard.writeText("aoEkHih");
-}
-
 //Alert
 function UpdateAlert(){
  alert("現在制作中です。");
