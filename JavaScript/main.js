958e1b16239202860a62274dc553eb0439ee3c3e
tapioka-jp <suetake0301@outlook.jp>
2025-04-26 10:28:18 +0900

    Update main.js

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -11,6 +11,15 @@ function USytbLink(){
  location.href= "https://tapioka-site.f5.si/Blog/UnwaveringSoul/UnwaveringSoul-YouTuber.html";
 }
 
+function USvideLink(){
+ location.href= "https://tapioka-site.f5.si/Blog/UnwaveringSoul/UnwaveringSoul-Video.html";
+}
+
+//Grace blog link
+function GCcideoLink(){
+ location.href= "https://tapioka-site.f5.si/Blog/Grace/Grace-Video.html";
+}
+
 //Alert
 function UpdateAlert(){
  alert("現在制作中です。");
