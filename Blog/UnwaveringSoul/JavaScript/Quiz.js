4439a42e683f009485941538621944a8e1fd26cc
tapioka-jp <suetake0301@outlook.jp>
2025-08-03 18:48:40 +0900

    Update Quiz.js

--- Blog/UnwaveringSoul/JavaScript/Quiz.js
+++ Blog/UnwaveringSoul/JavaScript/Quiz.js
@@ -7,6 +7,8 @@ function Answer(){
     QuizTitle.innerHTML = "第2問: 揺るぎない魂が作られたのはいつ？(例: 2025年8月3日)";
      if(Quiz.value == "2018年9月5日"){
        alert("正解です。揺るぎない魂が作られたのは2018年9月5n日です。\n2問目が表示されました！2問目も頑張りましょう！");
+     }else{
+       alert("Test massege");
      };
   }else{
     alert("不正解です。もう１度挑戦しましょう！");
