1190f7bc9495f8df346e833af8a140869a125d1e
tapioka-jp <suetake0301@outlook.jp>
2025-08-03 20:14:44 +0900

    Update 02.js

--- Blog/UnwaveringSoul/Quiz/JavaScript/02.js
+++ Blog/UnwaveringSoul/Quiz/JavaScript/02.js
@@ -2,8 +2,8 @@ var Quiz = document.getElementById("QuizForm");
 
 function Answer(){
   if(Quiz.value == "2018年9月5日"){
-    alert("正解です。揺るぎない魂が作られたのは、「2018年9月5日」です。。\n3問目に移動します！！3問目も頑張りましょう！\nまだ3問目は作れてないよ！");
-    //location.href= "03.html";
+    alert("正解です。揺るぎない魂が作られたのは、「2018年9月5日」です。。\n3問目に移動します！！3問目も頑張りましょう！");
+    location.href= "03.html";
   }else{
     alert("不正解です。もう１度挑戦しましょう！");
   };
