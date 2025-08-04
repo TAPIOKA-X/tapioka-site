4641eee95352ffe24ac4c1f56a211ff5290bbe16
tapioka-jp <suetake0301@outlook.jp>
2025-08-04 17:32:31 +0900

    Update 03.js

--- Blog/UnwaveringSoul/Quiz/JavaScript/03.js
+++ Blog/UnwaveringSoul/Quiz/JavaScript/03.js
@@ -2,8 +2,8 @@ var Quiz = document.getElementById("QuizForm");
 
 function Answer(){
   if(Quiz.value == "750000"){
-    alert("正解です。揺るぎない魂の武器「」のダメージは「750000(75万)」です。。\n4問目に移動します！！4問目も頑張りましょう！\nまだ4問目は作れてないよ！");
-    //location.href= "04.html";
+    alert("正解です。揺るぎない魂の武器「」のダメージは「750000(75万)」です。。\n4問目に移動します！！4問目も頑張りましょう！");
+    location.href= "04.html";
   }else{
     alert("不正解です。もう１度挑戦しましょう！");
   };
