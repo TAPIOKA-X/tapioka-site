07eceeaf5b3660fbbc16c3edcfacbc7978c53fe6
tapioka-jp <suetake0301@outlook.jp>
2025-08-04 17:49:58 +0900

    Update Point.js

--- Blog/UnwaveringSoul/Quiz/Point.js
+++ Blog/UnwaveringSoul/Quiz/Point.js
@@ -1,1 +1,11 @@
+var DT = document.title;
 
+if(DT === "Tester"){
+  alert("アクセスが不正です。");
+}else if(DT === "02"){
+  alert("アクセスが不正です。");
+}else if(DT === "32"){
+  alert("アクセスが不正です。");
+}else{
+  location.href= "https://tapioka-site.f5.si/";
+}
