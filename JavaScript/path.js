3ff123df3684b3bbe869fff44e3fa5ebeebd303d
tapioka-jp <suetake0301@outlook.jp>
2025-08-09 18:29:53 +0900

    Update path.js

--- JavaScript/path.js
+++ JavaScript/path.js
@@ -3,12 +3,18 @@ var USvideo1 = document.getElementById("KingSum");
 var USvideo2 = document.getElementById("AlphyNEO");
 var USvideo3 = document.getElementById("100Dummy");
 var USvideo4 = document.getElementById("SC-Sans");
+var RLvideo1 = document.getElementById("Rival-1");
+var RLvideo2 = document.getElementById("Rival-2");
+var RLvideo3 = document.getElementById("Rival-3");
 
 //動画のファイルパス
 var USvideo1_Link = "https://dl.dropboxusercontent.com/scl/fi/v8xgrqn7id2suas8t27gh/UnwaveringSoul-KingSum.mp4?rlkey=jmu1197f66isz43j9w4d04fqs&st=g93sebis&dl=0";
 var USvideo2_Link = "https://dl.dropboxusercontent.com/scl/fi/o32o8jsy42rl8dk7bexyc/UnwaveringSoul-AlphysNEO.mp4?rlkey=q3adpkimidd5mn2jk2xg8jrad&st=xpdf1f3d&dl=0";
 var USvideo3_Link = "https://tapioka-site.f5.si/Blog/UnwaveringSoul/Video/UnwaveringSoul-100Dummy.mp4";
 var USvideo4_Link = "https://onedrive.live.com/personal/8f195d6b34cc43d9/_layouts/15/download.aspx?SourceUrl=%2Fpersonal%2F8f195d6b34cc43d9%2FDocuments%2Fvideo%2FUnwaveringSoul%2DSC%2DSans%2Emp4";
+var RLvideo1_Link = "https://dl.dropboxusercontent.com/scl/fi/allqlkcvn3k8s9ujvnjh5/Rival-1v1-3.mp4?rlkey=w30c3u9rfavl7qeun9xbft6a8&st=g8b7etbu&dl=0";
+var RLvideo2_Link = "https://tapioka-site.f5.si/Blog/Rival/Video/Rival-1v1-5.mp4";
+var RLvideo3_Link = "https://dl.dropboxusercontent.com/scl/fi/vvv6nxtx4y290syh2a6f8/Rival-Acarde-Knife.mp4?rlkey=sycioa1c40mm3be7uyfofwgsg&st=8t1xn654&dl=0";
 
 window.onload = function(){
  if(document.title === "揺るぎない魂 動画"){
@@ -16,5 +22,9 @@ window.onload = function(){
    USvideo2.src = USvideo2_Link;
    USvideo3.src = USvideo3_Link;
    USvideo4.src = USvideo4_Link;
+ }else if(document.title === "ライバル 動画"){
+   RLvideo1.src = RLvideo1_Link;
+   RLvideo2.src = RLvideo2_Link;
+   RLvideo3.src = RLvideo3_Link;
  };  
 };
