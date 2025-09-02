bad175ec79ab3c5bb095359e1493a14971192c13
tapioka-jp <suetake0301@outlook.jp>
2025-09-02 15:58:36 +0900

    Update main.js

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -2,6 +2,8 @@
 const URL = {
  Home: "//tapioka-site.f5.si",
  Menu: "//tapioka-site.f5.si/Menu.html",
+ UsPs: "//tapioka-site.f5.si/Blog/UnwaveringSoul/UnwaveringSoul-PS.html",
+ YouTuber: "//tapioka-site.f5.si/Blog/YouTube/YouTuber.html",
  RobloxGroup: "//www.roblox.com/ja/communities/33917666/TAPIOKA#!/about",
  LINE: "//line.me/ti/g2/AgWjjh03PWGRbjgCPIfBie0e4-JIw4RES4T9Fg?utm_source=invitation&utm_medium=link_copy&utm_campaign=default"
 };
@@ -15,8 +17,22 @@ function MenuLink{
  location.href= URL.Menu;
 }
 
+function UnwaveringSoul_PrivateServer(){
+ location.href= URL.UsPs;
+}
+
+function LINE(){
+ window.open(URL.LINE,"_blank");
+}
 
 
+function RobloxGroup(){
+ window.open(URL.RobloxGroup,"_blank");
+}
+
+function YouTuber(){
+ location.href= URL.YouTuber;
+}
 
 
 //その他の関数
@@ -26,7 +42,6 @@ function PScodeCopy(){
 }
 
 function ShareLinkCopy(){
- const ShareLink = "//tapioka-site.f5.si/Share.html";
- navigator.clipboard.writeText(ShareLink);
+ navigator.clipboard.writeText(URL.Home);
  alert("コピーできました。");
 }
