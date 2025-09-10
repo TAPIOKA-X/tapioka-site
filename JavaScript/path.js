f8e0be15a0a7455c078d064f3ddd7c8de3c63f92
tapioka-jp <suetake0301@outlook.jp>
2025-09-10 21:08:57 +0900

    path

--- JavaScript/path.js
+++ JavaScript/path.js
@@ -1,13 +1,11 @@
-//Roblox Rivals
-let KillClip_1 = document.getElementById("Rivals-1");
 //Video URL
 const VideoURL = {
- KillClip_1: ""
+ Rivals_1: ""
 };
 
 //path
 window.onload = function(){
  if(location.href === "https://tapioka-site.f5.si/Blog/Rivals/Rivals-Video.html"){
-  KillClip_1.src = VideoURL.KillClip_1;
+  document.getElementById("Friend-2v2_1").src = VideoURL.Rivals_1;
  };
 };
