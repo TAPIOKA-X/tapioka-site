0094cc538b6ea6c3142644ae83c5a725c3e0c10d
tapioka-jp <suetake0301@outlook.jp>
2025-09-10 21:16:55 +0900

    Update path.js

--- JavaScript/path.js
+++ JavaScript/path.js
@@ -1,6 +1,6 @@
 //Video URL
 const VideoURL = {
- Rivals_1: ""
+ Rivals_1: "https://dl.dropboxusercontent.com/scl/fi/58k8gkajv4ckb40yts5c1/Friend-2v2_1.mp4?rlkey=5rdk11kesf5dkm8ww5934xtxa&st=beq9pnan&dl=0"
 };
 
 //path
