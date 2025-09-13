2904452b1b1cb2051cf91356b748a7592411f381
tapioka-jp <suetake0301@outlook.jp>
2025-09-13 20:32:33 +0900

    Update path.js

--- JavaScript/path.js
+++ JavaScript/path.js
@@ -1,7 +1,8 @@
 //Video URL
 const VideoURL = {
  Rivals_1: "https://dl.dropboxusercontent.com/scl/fi/58k8gkajv4ckb40yts5c1/Friend-2v2_1.mp4?rlkey=5rdk11kesf5dkm8ww5934xtxa&st=beq9pnan&dl=0",
- Rivals_2: "https://dl.dropboxusercontent.com/scl/fi/47c6ezr8szeuiuo70rgy7/vs-Blue_fool.mp4?rlkey=j993wipxfqr2lsf9do2qiepy2&st=qwfmaynp&dl=0"
+ Rivals_2: "https://dl.dropboxusercontent.com/scl/fi/47c6ezr8szeuiuo70rgy7/vs-Blue_fool.mp4?rlkey=j993wipxfqr2lsf9do2qiepy2&st=qwfmaynp&dl=0",
+ Rivals_3: "https://dl.dropboxusercontent.com/scl/fi/d86ak6na2ruzcqy8qmiux/Kill-Clip_1.mp4?rlkey=g54otifolj1f00xuc46b1t6h5&st=rivxzy2v&dl=0"
 };
 
 //path
@@ -9,5 +10,6 @@ window.onload = function(){
  if(location.href === "https://tapioka-site.f5.si/Blog/Rivals/Rivals-Video.html"){
   document.getElementById("Friend-2v2_1").src = VideoURL.Rivals_1;
    document.getElementById("vsBlue_fool").src = VideoURL.Rivals_2;
+    document.getElementById("Kill-Clip_1").src = VideoURL.Rivals_3;
  };
 };
