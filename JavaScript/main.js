6a1a03ab9373deebb327fdb319d3d9c40fa42d6e
tapioka-jp <suetake0301@outlook.jp>
2025-10-01 16:13:53 +0900

    Update main.js

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -5,7 +5,7 @@ Menu:"https://tapioka-site.f5.si/Menu.html",
 UsPs:"https://tapioka-site.f5.si/Blog/UnwaveringSoul/UnwaveringSoul-PS.html",
 Rivals_Video:"https://tapioka-site.f5.si/Blog/Rivals/Rivals-Video.html",
 YouTuber:"https://tapioka-site.f5.si/Blog/YouTube/YouTuber.html",
-YouTuber:"https://tapioka-site.f5.si/Blog/Tools/TS-Editor.html",
+TS_Editor:"https://tapioka-site.f5.si/Blog/Tools/TS-Editor.html",
 RobloxGroup:"https://www.roblox.com/ja/communities/33917666/TAPIOKA#!/about",
 LINE:"https://line.me/ti/g2/AgWjjh03PWGRbjgCPIfBie0e4-JIw4RES4T9Fg?utm_source=invitation&utm_medium=link_copy&utm_campaign=default"
 };
