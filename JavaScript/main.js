40be3e37d64be4455c5775cc1358d0c520894334
tapioka-jp <suetake0301@outlook.jp>
2025-11-03 11:41:01 +0900

    Update main.js

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,8 +1,23 @@
-const URL = {
-RobloxGroup:"https://www.roblox.com/ja/communities/33917666/TAPIOKA#!/about",
-LINE:"https://line.me/ti/g2/AgWjjh03PWGRbjgCPIfBie0e4-JIw4RES4T9Fg?utm_source=invitation&utm_medium=link_copy&utm_campaign=default"
+function DeviceType(){
+let AdsURL = {
+ Phone : "https://adm.shinobi.jp/s/30676a41c8f9c49d8f43aaba667976c5",
+ PC : "https://adm.shinobi.jp/s/1ba4cd8884e62c1d123487c7fbccde9e"
 };
-function PScodeCopy(){
+ const w = window.innerWidth;
+ const h = window.innerHeight;
+  let Ads = document.getElementsByClassName("Ads");
+if(w > 467 && h > 59){
+ Ads.src = AdsURL.PC;
+}else if(w < 319 && h < 49){
+ Ads.src = AdsURL.Phone;
+}else{
+ Ads.src = AdsURL.PC;
+}
+}
+
+window.onload = DeviceType;
+
+function PScodeCopy(){ //ゆるたまのプラサバのやつ
  let PScode = document.getElementById("US-PS").textContent;
  navigator.clipboard.writeText(PScode);
  alert(`プライベートサーバーコード「${PScode}」をコピーできました。`);
