720a9514b8af1e65e4b249612fa9f786ce911e59
tapioka-jp <suetake0301@outlook.jp>
2025-11-03 12:00:31 +0900

    Remove device type detection for ads
    Removed device type detection and ad URL assignment logic.

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,23 +1,3 @@
-window.onload = () => {
-　setTimeout(() => {
-function DeviceType(){
-let AdsURL = {
- Phone : "https://adm.shinobi.jp/s/30676a41c8f9c49d8f43aaba667976c5",
- PC : "https://adm.shinobi.jp/s/1ba4cd8884e62c1d123487c7fbccde9e"
-};
- const w = window.innerWidth;
- const h = window.innerHeight;
-  let Ads = document.getElementsByClassName("Ads");
-if(w > 467 && h > 59){
- Ads[0].src = AdsURL.PC;
-}else if(w < 319 && h < 49){
- Ads[0].src = AdsURL.Phone;
-}else{
- Ads[0].src = AdsURL.PC;
-}
-}
-},100);
-}
 function PScodeCopy(){ //ゆるたまのプラサバのやつ
  let PScode = document.getElementById("US-PS").textContent;
  navigator.clipboard.writeText(PScode);
