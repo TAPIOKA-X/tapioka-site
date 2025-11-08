059709708d47ce118f6c37d3351f8b4b2dbf9773
tapioka-jp <suetake0301@outlook.jp>
2025-11-08 10:41:58 +0900

    Update main.js

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,5 +1,10 @@
+ let PScodeElement = document.getElementById("US-PS");
+ const PScode = "28d14zT";
+
+window.onload = function(){
+ PScodeElement.textContent = PScode;
+}
 function PScodeCopy(){ //ゆるたまのプラサバのやつ
- let PScode = document.getElementById("US-PS").textContent;
- navigator.clipboard.writeText(PScode);
+ navigator.clipboard.writeText(PScode;
  alert(`プライベートサーバーコード「${PScode}」をコピーできました。`);
 }
